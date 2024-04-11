685bdc732b59c31391ae3e376ed1c0a062429e81
wermoss <konrad@minister.com>
2024-04-11 14:13:55 +0200

    text test

--- kancelaria/pages/back.vue
+++ kancelaria/pages/back.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="w-screen bg-green-100 flex items-center justify-center">
         <div class="max-w-screen-2xl w-full bg-pink-100 bg-no-repeat bg-center bg-cover bg-[url('https://www.lubinandmeyer.com/proto/images/lubin-courtroom.jpg')]">
-            <div class="bg-black/50 h-screen">
-                <div class="container mx-auto text-white p-20">Kancelara adwokacka</div>
+            <div class="bg-black/50 h-screen flex flex-col items-center justify-center">
+                <div class="container mx-auto text-white p-4 bg-pink-100">Kancelara adwokacka</div>
+                <div class="container mx-auto text-white p-4 bg-pink-100">Zawsze po Twojej stronie</div>
+
             </div>
         </div>
     </div>
