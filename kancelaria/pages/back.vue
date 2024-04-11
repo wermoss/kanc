f37aaa59c360c297622be879f0dcac2ba51de2b6
wermoss <konrad@minister.com>
2024-04-11 13:35:25 +0200

    Background test

---
+++ kancelaria/pages/back.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-screen bg-green-100 flex items-center justify-center">
+        <div class="max-w-screen-2xl h-screen bg-pink-100 bg-no-repeat bg-center	bg-cover bg-[url('https://www.lubinandmeyer.com/proto/images/lubin-courtroom.jpg')]">
+            aaa fgdgdsg sdfgd fdsg fdsg fdg dfg df gdf gdf gdf gsd fdsf sdfs dfsd fsd fsd fds fsd fsd fsd fsd fsd fsd fsd fsd ff gdfg dfg dsfgsd gdfgdf gdfg fdsg dsg dsg df gdf gdf gdf gsdfg dfgd gdfsg 
+        </div>
+    </div>
+</template>
+
+<script></script>
+<style></style>
--- kancelaria/pages/index.vue
+++ kancelaria/pages/index.vue
@@ -1,14 +1,16 @@
+
 <template>
-  <div class="w-screen h-screen flex items-center justify-center bg-pink-100">
+<div class="w-screen flex items-center">
+  <div class="max-w-screen-md h-screen flex items-center justify-center bg-no-repeat bg-center	bg-cover bg-[url('https://www.lubinandmeyer.com/proto/images/lubin-courtroom.jpg')]">
     <div class="container mx-auto bg-gray-100">
       dsdsfds
      <Square />
     </div>
   </div>
+</div>
 </template>
 
 <script setup>
 </script>
 
-<style scoped>
-</style>
+<style></style>
