a56b90c0fb3e836c87270d677d6b546069f724a6
wermoss <konrad@minister.com>
2024-04-11 14:52:03 +0200

    Menu

--- kancelaria/components/header.vue
+++ kancelaria/components/header.vue
@@ -1,3 +1,24 @@
 <template>
-<div>Header</div>
-</template>
+    <div class="w-screen flex items-center justify-center absolute">
+        <div class="max-w-screen-2xl w-full">
+                <div class="container mx-auto text-white p-4 flex justify-between">
+                    <div class="">Logo Here</div>
+                    <div class="">
+                        <ul class="flex gap-4">
+                            <li>
+                                Home
+                            </li>
+                            <li>
+                                Kancelaria
+                            </li>
+                            <li>
+                                Blog
+                            </li>
+                            <li>
+                                Kontakt
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+        </div>
+    </div></template>
