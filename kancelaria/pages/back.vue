e7e3550b0e1ff1f2ccf7c181e5093baba1f93423
wermoss <konrad@minister.com>
2024-04-12 23:12:07 +0200

    upscale background

--- kancelaria/pages/back.vue
+++ kancelaria/pages/back.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full bg-green-100 flex items-center justify-center bg-no-repeat bg-center bg-cover bg-[url('https://res.cloudinary.com/dqoftbaal/image/upload/f_auto,q_auto/1502076_LawyerStyleMen_Blog-Post-Hero-Image_1200X630_120522_gfmj8x')]">
+    <div class="w-full bg-green-100 flex items-center justify-center bg-no-repeat bg-center bg-cover bg-[url('https://res.cloudinary.com/dqoftbaal/image/upload/f_auto,q_auto/lawyer_jlwvcn')]">
         <div class="max-w-screen-2xl w-full  ">
             <div class=" h-screen flex flex-col items-center justify-center text-white">
                 <div class="container mx-auto text-xl p-4">
