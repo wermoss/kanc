1b4baa99a3a7648f7555b82eff5b712fa44fa40b
wermoss <konrad@minister.com>
2024-04-14 11:18:44 +0200

    svh

--- kancelaria/pages/back.vue
+++ kancelaria/pages/back.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full bg-green-100 flex items-center justify-center bg-no-repeat bg-center bg-cover bg-[url('https://res.cloudinary.com/dqoftbaal/image/upload/f_auto,q_auto/lawyer_jlwvcn')]">
     <div class="max-w-screen-2xl w-full">
-      <div class="h-screen flex flex-col items-center justify-center text-white">
+      <div class="h-svh flex flex-col items-center justify-center text-white">
         <div class="container mx-auto text-xl px-8">
           <div><Subtitle /></div><br><br><br>
           <div><Zombie /></div>
