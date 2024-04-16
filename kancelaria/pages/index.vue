52128cd94d7ec0f6d721966a6605f601df620797
Arkadiusz Tepper <tepirek@outlook.com>
2024-04-16 15:23:30 +0200

    clear cookies;

--- kancelaria/pages/index.vue
+++ kancelaria/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="w-full flex items-center">
     <div class="w-screen h-screen flex items-center justify-center">
       <div>
-        <h1>Version: 1.0.0</h1>
+        <h1>Version: 1.0.1</h1>
         <div>
           <CookieControl />
         </div>
@@ -28,11 +28,12 @@ watch(
     () => cookiesEnabledIds.value,
     (current, previous) => {
       if (current?.includes('_ga') !== previous?.includes('_ga')) {
+        const eraseCookie = (name) => {
+          document.cookie = name + '=; Max-Age=0';
+        };
         console.log(current);
-        const _gaCookie = useCookie('_ga');
-        _gaCookie.value = null;
-        const _gaContainerCookie = useCookie('_ga_5FK73WVJ8L');
-        _gaContainerCookie.value = null;
+        eraseCookie('_ga');
+        eraseCookie('_ga_5FK73WVJ8L');
         // cookie with id `google-analytics` got added
         window.location.reload(); // placeholder for your custom change handler
       }
