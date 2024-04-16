0be9956f978af572e33ea5ee8cf9f8e9072171c0
Arkadiusz Tepper <tepirek@outlook.com>
2024-04-16 15:58:17 +0200

    reload always;

--- kancelaria/pages/index.vue
+++ kancelaria/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="w-full flex items-center">
     <div class="w-screen h-screen flex items-center justify-center">
       <div>
-        <h1>Version: 1.0.3</h1>
+        <h1>Version: 1.0.4</h1>
         <div>
           <CookieControl />
         </div>
@@ -28,16 +28,14 @@ const {
 watch(
     () => cookiesEnabledIds.value,
     (current, previous) => {
-      if (current?.includes('gtm') !== previous?.includes('gtm')) {
-        const eraseCookie = (name) => {
-          document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.lexxo.vercel.app;';
-        };
-        console.log(current);
-        eraseCookie('_ga');
-        eraseCookie('_ga_5FK73WVJ8L');
-        // cookie with id `google-analytics` got added
-        window.location.reload(); // placeholder for your custom change handler
-      }
+      const eraseCookie = (name) => {
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.lexxo.vercel.app;';
+      };
+      console.log(current);
+      eraseCookie('_ga');
+      eraseCookie('_ga_5FK73WVJ8L');
+      // cookie with id `google-analytics` got added
+      window.location.reload(); // placeholder for your custom change handler
     },
     { deep: true },
 );
