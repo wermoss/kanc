f051b233857245db70f981c6143b629758b49f13
Arkadiusz Tepper <tepirek@outlook.com>
2024-04-16 15:17:38 +0200

    clear cookies;

--- kancelaria/pages/index.vue
+++ kancelaria/pages/index.vue
@@ -1,16 +1,17 @@
-
 <template>
-<div class="w-full flex items-center">
-  <div class="w-screen h-screen flex items-center justify-center">
-    <div>
-      
-      <div><CookieControl /></div>
-<!--      <div><Geolocation /></div>-->
-<!--      <div><Browser /></div>-->
+  <div class="w-full flex items-center">
+    <div class="w-screen h-screen flex items-center justify-center">
+      <div>
 
+        <div>
+          <CookieControl />
+        </div>
+        <!--      <div><Geolocation /></div>-->
+        <!--      <div><Browser /></div>-->
+
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script setup lang="ts">
@@ -24,13 +25,19 @@ const {
 
 // example: react to a cookie being accepted
 watch(
-  () => cookiesEnabledIds.value,
-  (current, previous) => {
-    if (current?.includes('_ga') !== previous?.includes('_ga')) {
-      // cookie with id `google-analytics` got added
-      window.location.reload() // placeholder for your custom change handler
-    }
-  },
-  { deep: true },
-)
+    () => cookiesEnabledIds.value,
+    (current, previous) => {
+      if (current?.includes('_ga') !== previous?.includes('_ga')) {
+        if (!current?.includes('_ga')) {
+          const _gaCookie = useCookie('_ga');
+          _gaCookie.value = null;
+          const _gaContainerCookie = useCookie('_ga_5FK73WVJ8L');
+          _gaContainerCookie.value = null;
+        }
+        // cookie with id `google-analytics` got added
+        window.location.reload(); // placeholder for your custom change handler
+      }
+    },
+    { deep: true },
+);
 </script>
