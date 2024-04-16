48cba7296e740b72f3e4adebb6d47d2756c9b76a
Arkadiusz Tepper <tepirek@outlook.com>
2024-04-16 16:21:58 +0200

    clear cookies;

--- kancelaria/nuxt.config.ts
+++ kancelaria/nuxt.config.ts
@@ -1,45 +1,44 @@
-
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: 'Playground',
-      meta: [
-        { hid: 'robots', name: 'robots', content: 'noindex' }
-      ]
-    },
-  },
-  modules: ['@dargmuesli/nuxt-cookie-control', '@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
-  typescript: {
-    includeWorkspace: true,
-  },
-
-  // module options
-  cookieControl: {
-    colors: {
-      checkboxActiveBackground: '#00A34A', // text-green-600
-    },
-    closeModalOnClickOutside: true,
-    cookies: {
-      necessary: [],
-      optional: [
-        {
-          name: 'Google Tag Manager',
-          id: 'gtm',
-          targetCookieIds: ['_ga', '_ga_5FK73WVJ8L'],
+    app: {
+        head: {
+            title: 'Playground',
+            meta: [
+                { hid: 'robots', name: 'robots', content: 'noindex' },
+            ],
         },
-      ],
     },
-    isCookieIdVisible: true,
-    locales: ['en', 'de'],
-    localeTexts: {
-      de: {
-        iframeBlocked: 'Bitte funktionale Cookies aktivieren:',
-      },
+    modules: [ '@dargmuesli/nuxt-cookie-control', '@nuxtjs/tailwindcss', '@nuxtjs/i18n' ],
+    typescript: {
+        includeWorkspace: true,
     },
-  },
-})
+    ssr: false,
+    // module options
+    cookieControl: {
+        colors: {
+            checkboxActiveBackground: '#00A34A', // text-green-600
+        },
+        closeModalOnClickOutside: true,
+        cookies: {
+            necessary: [],
+            optional: [
+                {
+                    name: 'Google Tag Manager',
+                    id: 'gtm',
+                    targetCookieIds: [ '_ga', '_ga-<token>' ],
+                },
+            ],
+        },
+        isCookieIdVisible: false,
+        locales: [ 'en', 'de' ],
+        localeTexts: {
+            de: {
+                iframeBlocked: 'Bitte funktionale Cookies aktivieren:',
+            },
+        },
+    },
+});
 //   css: [
 //     '~/assets/css/global.css'
 //   ],
-  
+
 // })
--- kancelaria/plugins/gtm.client.js
+++ kancelaria/plugins/gtm.client.js
@@ -8,11 +8,14 @@ export default defineNuxtPlugin((nuxtApp) => {
         return;
     }
 
+    const {$cookies} = useNuxtApp()
+    console.log($cookies)
+
     nuxtApp.vueApp.use(createGtm({
         id: 'GTM-59LPWQ79',
         defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
         compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
-        nonce: '2726c7f26c', // Will add `nonce` to the script tag
+        // nonce: '2726c7f26c', // Will add `nonce` to the script tag
         enabled: !!cookieControl.cookiesEnabledIds.value?.includes('gtm'), // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
         debug: true, // Whether or not display console logs debugs (optional)
         loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
