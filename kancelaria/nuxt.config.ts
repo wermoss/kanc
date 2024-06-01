9cb6c11af293bdd41237ff0811aa4a59645e54ed
Arkadiusz Tepper <tepirek@outlook.com>
2024-06-01 20:52:06 +0200

    update cookies

--- kancelaria/nuxt.config.ts
+++ kancelaria/nuxt.config.ts
@@ -1,64 +1,102 @@
 export default defineNuxtConfig({
-    app: {
-        head: {
-            title: 'Playground',
-            meta: [
-                { hid: 'robots', name: 'robots', content: 'noindex' },
-            ],
-        },
+  app: {
+    head: {
+      title: "Playground",
+      meta: [{ hid: "robots", name: "robots", content: "noindex" }],
     },
-    modules: [
-        '@dargmuesli/nuxt-cookie-control',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/i18n',
-        'nuxt-gtag',
+  },
+  modules: [
+    "@dargmuesli/nuxt-cookie-control",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+    "nuxt-gtag",
+  ],
+  gtag: {
+    id: "G-5FK73WVJ8L",
+    enabled: false,
+    initCommands: [
+      // Setup up consent mode
+      [
+        "consent",
+        "default",
+        {
+          ad_user_data: "denied",
+          ad_personalization: "denied",
+          ad_storage: "denied",
+          analytics_storage: "denied",
+          wait_for_update: 500,
+        },
+      ],
     ],
-    gtag: {
-        id: 'G-5FK73WVJ8L',
-        enabled: false,
-        initCommands: [
-            // Setup up consent mode
-            [ 'consent', 'default', {
-                ad_user_data: 'denied',
-                ad_personalization: 'denied',
-                ad_storage: 'denied',
-                analytics_storage: 'denied',
-                wait_for_update: 500,
-            } ],
-        ],
+  },
+  typescript: {
+    includeWorkspace: true,
+  },
+  ssr: false,
+  // module options
+  cookieControl: {
+    colors: {
+      checkboxActiveBackground: "#00A34A", // text-green-600
     },
-    typescript: {
-        includeWorkspace: true,
+    closeModalOnClickOutside: true,
+    cookies: {
+      necessary: [],
+      optional: [
+        // {
+        //   name: "Google Tag Manager",
+        //   id: "gtm",
+        //   targetCookieIds: ["_ga", "_ga-<token>"],
+        //   cookies: ["_ga", "_ga_5FK73WVJ8L"],
+        //   accepted: () => {
+        //     console.log("Google Tag Manager accepted");
+        //   },
+        // },
+        {
+          name: "User Data",
+          id: "user-data",
+          targetCookieIds: ["_ga", "_ga-<token>"],
+          cookies: ["_ga", "_ga_5FK73WVJ8L"],
+          accepted: () => {
+            console.log("Google Tag Manager accepted");
+          },
+        },
+        {
+          name: "Personalization",
+          id: "personalization",
+          targetCookieIds: ["_ga", "_ga-<token>"],
+          cookies: ["_ga", "_ga_5FK73WVJ8L"],
+          accepted: () => {
+            console.log("Google Tag Manager accepted");
+          },
+        },
+        {
+          name: "Storage",
+          id: "storage",
+          targetCookieIds: ["_ga", "_ga-<token>"],
+          cookies: ["_ga", "_ga_5FK73WVJ8L"],
+          accepted: () => {
+            console.log("Google Tag Manager accepted");
+          },
+        },
+        {
+          name: "Analytics",
+          id: "analytics",
+          targetCookieIds: ["_ga", "_ga-<token>"],
+          cookies: ["_ga", "_ga_5FK73WVJ8L"],
+          accepted: () => {
+            console.log("Google Tag Manager accepted");
+          },
+        },
+      ],
     },
-    ssr: false,
-    // module options
-    cookieControl: {
-        colors: {
-            checkboxActiveBackground: '#00A34A', // text-green-600
-        },
-        closeModalOnClickOutside: true,
-        cookies: {
-            necessary: [],
-            optional: [
-                {
-                    name: 'Google Tag Manager',
-                    id: 'gtm',
-                    targetCookieIds: [ '_ga', '_ga-<token>' ],
-                    cookies: [ '_ga', '_ga_5FK73WVJ8L' ],
-                    accepted: () => {
-                        console.log('Google Tag Manager accepted');
-                    },
-                },
-            ],
-        },
-        isCookieIdVisible: false,
-        locales: [ 'en', 'de' ],
-        localeTexts: {
-            de: {
-                iframeBlocked: 'Bitte funktionale Cookies aktivieren:',
-            },
-        },
+    isCookieIdVisible: false,
+    locales: ["en", "de"],
+    localeTexts: {
+      de: {
+        iframeBlocked: "Bitte funktionale Cookies aktivieren:",
+      },
     },
+  },
 });
 //   css: [
 //     '~/assets/css/global.css'
--- kancelaria/pages/index.vue
+++ kancelaria/pages/index.vue
@@ -8,14 +8,12 @@
         </div>
         <!--      <div><Geolocation /></div>-->
         <!--      <div><Browser /></div>-->
-
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
 const {
   cookiesEnabled,
   cookiesEnabledIds,
@@ -26,34 +24,62 @@ const {
 
 function allConsentGranted() {
   const { gtag } = useGtag();
-  gtag('consent', 'update', {
-    ad_user_data: 'granted',
-    ad_personalization: 'granted',
-    ad_storage: 'granted',
-    analytics_storage: 'granted',
+  gtag("consent", "update", {
+    ad_user_data: "granted",
+    ad_personalization: "granted",
+    ad_storage: "granted",
+    analytics_storage: "granted",
   });
 }
 
 const eraseCookie = (name) => {
-  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.lexxo.vercel.app;Max-Age=0;';
+  document.cookie =
+    name +
+    "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.lexxo.vercel.app;Max-Age=0;";
 };
 
 // example: react to a cookie being accepted
 watch(
-    () => cookiesEnabledIds.value,
-    (current) => {
-      if (current?.includes('gtm')) {
-        const { initialize } = useGtag();
-        initialize();
-        allConsentGranted();
-      } else {
-        eraseCookie('_ga');
-        eraseCookie('_ga_5FK73WVJ8L');
-      }
+  () => cookiesEnabledIds.value,
+  (current) => {
+    console.log(current);
 
-      // cookie with id `google-analytics` got added
-      window.location.reload(); // placeholder for your custom change handler
-    },
-    { deep: true },
+    const { initialize, gtag } = useGtag();
+
+    initialize();
+
+    const permissions = {};
+
+    if (current?.includes("user-data")) {
+      permissions.ad_user_data = "granted";
+    }
+
+    if (current?.includes("personalization")) {
+      permissions.ad_personalization = "granted";
+    }
+
+    if (current?.includes("storage")) {
+      permissions.ad_storage = "granted";
+    }
+
+    if (current?.includes("analytics")) {
+      permissions.analytics_storage = "granted";
+    }
+
+    gtag("consent", "update", permissions);
+
+    // if (current?.includes("gtm")) {
+    //   const { initialize } = useGtag();
+    //   initialize();
+    //   allConsentGranted();
+    // } else {
+    //   eraseCookie("_ga");
+    //   eraseCookie("_ga_5FK73WVJ8L");
+    // }
+
+    // cookie with id `google-analytics` got added
+    window.location.reload(); // placeholder for your custom change handler
+  },
+  { deep: true }
 );
 </script>
